1df8731864ba2ba39fb7bf497dd03aef9b7e9d7a
GinevraGE <117026341+GinevraGE@users.noreply.github.com>
2022-12-26 03:33:32 +0100

    commented out unit conversion

--- src/index.js
+++ src/index.js
@@ -136,33 +136,33 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function showFahrenheitTemp(event) {
-  event.preventDefault();
+//function showFahrenheitTemp(event) {
+//event.preventDefault();
 
-  let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
-}
+//let temperatureElement = document.querySelector("#temperature");
+//celsiusLink.classList.remove("active");
+//fahrenheitLink.classList.add("active");
+//let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32;
+// temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+//}
 
-function showCelsiusTemp(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
+//function showCelsiusTemp(event) {
+// event.preventDefault();
+//celsiusLink.classList.add("active");
+//fahrenheitLink.classList.remove("active");
+//let temperatureElement = document.querySelector("#temperature");
+//temperatureElement.innerHTML = Math.round(celsiusTemperature);
+//}
 
-let celsiusTemperature = null;
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
+//let celsiusTemperature = null;
+//let form = document.querySelector("#search-form");
+//form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", showFahrenheitTemp);
+//let fahrenheitLink = document.querySelector("#fahrenheit-link");
+//fahrenheitLink.addEventListener("click", showFahrenheitTemp);
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", showCelsiusTemp);
+//let celsiusLink = document.querySelector("#celsius-link");
+//celsiusLink.addEventListener("click", showCelsiusTemp);
 
 search("Parma");
 
